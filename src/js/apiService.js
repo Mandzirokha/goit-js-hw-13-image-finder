a8228f125e45707c0781ab13a5805a8fb1ab1e11
Mandzirokha <hanna.mandzirokha@gmail.com>
2021-09-25 21:42:47 +0300

    add files

---
+++ src/js/apiService.js
@@ -0,0 +1,48 @@
+const API_KEY = '23559602-074962a3d1482825ece0a5bf8';
+const BASE_URL = 'https://pixabay.com/api/';
+
+const options = {
+  headers: {
+    Authorization: API_KEY,
+  },
+};
+
+export default class ApiService {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+    this.key = '23559602-074962a3d1482825ece0a5bf8';
+  }
+
+  fetchArticles() {
+    const url = `${BASE_URL}?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=12&key=${this.key}`;
+
+    return fetch(url, options)
+      .then(response => {
+        console.log(response);
+        return response.json();
+      })
+      .then(({ articles }) => {
+        console.log(articles);
+        this.incrementPage();
+
+        return articles;
+      });
+  }
+
+  incrementPage() {
+    this.page += 1;
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+
+  get query() {
+    return this.searchQuery;
+  }
+
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
+}
---
+++ src/js/searchImage.js
@@ -0,0 +1,51 @@
+import ApiService from './apiService';
+import articlesTpl from '../templates/image.hbs';
+import LoadMoreBtn from './loadMoreBtn';
+
+const refs = {
+  searchForm: document.querySelector('.search-form'),
+  //   loadMoreBtn: document.querySelector('[data-action="load-more"]'),
+  articlesContainer: document.querySelector('.gallery'),
+};
+
+const loadMoreBtn = new LoadMoreBtn({
+  selector: '[data-action="load-more"]',
+  hidden: true,
+});
+const apiService = new ApiService();
+
+refs.searchForm.addEventListener('submit', onSearch);
+// refs.loadMoreBtn.addEventListener('click', onLoadMore);
+loadMoreBtn.refs.button.addEventListener('click', fetchArticles);
+
+function onSearch(e) {
+  e.preventDefault();
+
+  apiService.query = e.currentTarget.elements.query.value;
+
+  if (apiService.query === '') {
+    return alert('неправильный ввод');
+  }
+
+  loadMoreBtn.show();
+  apiService.resetPage();
+  // appendArticlesMarkup(articles);
+  clearArticlesContainer();
+  fetchArticles();
+}
+
+function fetchArticles() {
+  loadMoreBtn.disable();
+  apiService.fetchArticles().then(articles => {
+    appendArticlesMarkup(articles);
+    loadMoreBtn.enable();
+  });
+}
+
+function appendArticlesMarkup(articles) {
+  refs.articlesContainer.insertAdjacentHTML('beforeend', articlesTpl(articles));
+}
+
+function clearArticlesContainer() {
+  refs.articlesContainer.innerHTML = '';
+}
