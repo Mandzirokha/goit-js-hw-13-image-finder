424f3f43fb44931415d471de76e422e95d823d02
Mandzirokha <hanna.mandzirokha@gmail.com>
2021-09-26 03:07:52 +0300

    deleted no need files

--- src/js/apiService.js
+++ src/js/apiService.js
@@ -4,7 +4,7 @@ export default class NewApiService {
     (this.searchQuery = ''), (this.page = 1);
   }
   fetchArticles() {
-    const url = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=8&key=${API_KEY}`;
+    const url = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=12&key=${API_KEY}`;
     return fetch(url)
       .then(response => response.json())
       .then(({ hits }) => {
