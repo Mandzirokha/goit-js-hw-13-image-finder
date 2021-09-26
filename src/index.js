d99291689a00664ad23591c978213cf707b601a6
Mandzirokha <hanna.mandzirokha@gmail.com>
2021-09-26 03:04:40 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,4 +1,96 @@
 import './sass/main.scss';
 
-// import './js/apiService';
-import './js/searchImage';
+import NewApiService from './js/apiService';
+import LoadMoreBtn from './js/loadMoreBtn';
+import articleTpl from './templates/image.hbs';
+
+import { success, error } from '@pnotify/core';
+import '@pnotify/core/dist/PNotify.css';
+import '@pnotify/core/dist/BrightTheme.css';
+
+import * as basicLightbox from 'basiclightbox';
+import '../node_modules/basiclightbox/dist/basicLightbox.min.css';
+
+const refs = {
+  searchForm: document.querySelector('.search-form'),
+  articlesContainer: document.querySelector('.gallery'),
+  loadMoreBtn: document.querySelector('.my-element-selector'),
+};
+
+const newApiService = new NewApiService();
+
+const loadMoreBtn = new LoadMoreBtn({
+  selector: '[data-action="load-more"]',
+  hidden: true,
+});
+
+refs.searchForm.addEventListener('submit', onSearch);
+loadMoreBtn.refs.button.addEventListener('click', fetchArticles);
+
+function onSearch(e) {
+  e.preventDefault();
+  newApiService.query = e.currentTarget.elements.query.value;
+  if (newApiService.query.length === 0) {
+    onFetchError();
+    return;
+  }
+
+  loadMoreBtn.show();
+  newApiService.resetPage();
+  clearArticlesContainer();
+  fetchArticles();
+}
+
+function fetchArticles() {
+  loadMoreBtn.disable();
+  newApiService.fetchArticles().then(articles => {
+    appendArticleMarkup(articles);
+    loadMoreBtn.enable();
+  });
+}
+
+function appendArticleMarkup(data) {
+  // console.log(data[4].largeImageURL);
+  console.log(data.length);
+  if (data.length < 8) {
+    onFetchError();
+    clearArticlesContainer();
+    return;
+  }
+  onFetchSuccess();
+  refs.articlesContainer.insertAdjacentHTML('beforeend', articleTpl(data));
+  refs.articlesContainer.addEventListener('click', myImageBig);
+  loadMoreBtn.refs.button.scrollIntoView({
+    block: 'end',
+    behavior: 'smooth',
+  });
+}
+
+function clearArticlesContainer() {
+  refs.articlesContainer.innerHTML = '';
+}
+
+function myImageBig(data) {
+  data.preventDefault();
+  if (data.target.nodeName !== 'IMG') {
+    return;
+  }
+  const clickImg = data.target.dataset.large;
+
+  console.log(clickImg);
+  basicLightbox.create(`<img width="1400" height="900" src="${clickImg}">`).show();
+}
+
+function onFetchSuccess() {
+  success({
+    title: 'Succsess!',
+    delay: 4000,
+  });
+}
+
+function onFetchError() {
+  error({
+    title: 'Error!',
+    delay: 4000,
+  });
+}
--- src/js/apiService.js
+++ src/js/apiService.js
@@ -1,46 +1,27 @@
 const API_KEY = '23559602-074962a3d1482825ece0a5bf8';
-const BASE_URL = 'https://pixabay.com/api/';
-
-const options = {
-  headers: {
-    Authorization: API_KEY,
-  },
-};
-
-export default class ApiService {
+export default class NewApiService {
   constructor() {
-    this.searchQuery = '';
-    this.page = 1;
+    (this.searchQuery = ''), (this.page = 1);
   }
-
   fetchArticles() {
-    const url = `${BASE_URL}?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=12&key=${API_KEY}`;
-
-    return fetch(url, options)
-      .then(response => {
-        console.log(response);
-        return response.json();
-      })
-      .then(({ articles }) => {
-        console.log(articles);
+    const url = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=8&key=${API_KEY}`;
+    return fetch(url)
+      .then(response => response.json())
+      .then(({ hits }) => {
         this.incrementPage();
-
-        return articles;
+        return hits;
       });
   }
 
   incrementPage() {
     this.page += 1;
   }
-
   resetPage() {
     this.page = 1;
   }
-
   get query() {
     return this.searchQuery;
   }
-
   set query(newQuery) {
     this.searchQuery = newQuery;
   }
